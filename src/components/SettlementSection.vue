3aa57541e6b6b48e61af1ecdab73ea317ad3868e
Andrew N. Markin <andrew.n.markin@gmail.com>
2026-09-16 22:23:54 +0300

    Ability to copy settlements as text instructions added

--- src/components/SettlementSection.vue
+++ src/components/SettlementSection.vue
@@ -2,10 +2,12 @@
   <section>
     <div class="row items-center">
       <span class="text-h5 text-primary q-mr-xs q-my-sm">Settlements</span>
+      <q-btn flat round color="primary" icon="mdi-content-copy" @click="copy()" />
     </div>
     <q-markup-table separator="cell" flat bordered>
       <thead>
         <tr>
+          <th><q-checkbox v-model="selectAll" :disable="settlements.length === 0" /></th>
           <th class="text-left">Sender</th>
           <th class="text-left">Receiver</th>
           <th class="text-left w-100">Receiver Transfer Preferences</th>
@@ -13,26 +15,27 @@
         </tr>
       </thead>
       <tbody>
-        <tr
-          v-for="settlement in settlementsExtended"
-          :key="settlement.id"
-          @click="edit(settlement)"
-        >
+        <tr v-for="(settlement, index) in settlements" :key="index" @click="edit(settlement)">
+          <td><q-checkbox v-model="selection" :val="settlement" /></td>
           <td class="text-left">
             <participant-label :id="settlement.sender" />
           </td>
           <td class="text-left">
             <participant-label :id="settlement.receiver" />
           </td>
-          <td>
-            {{ settlement.preferences }}
+          <td class="text-left">
+            <participant-label :id="settlement.receiver">
+              <template #default="{ participant }">
+                {{ participant.preferences }}
+              </template>
+            </participant-label>
           </td>
           <td class="text-right">
             {{ settlement.amount }}
           </td>
         </tr>
         <tr v-if="settlements.length === 0">
-          <td colspan="4" class="muted-1 text-center q-td--no-hover">No settlements</td>
+          <td colspan="5" class="muted-1 text-center q-td--no-hover">No settlements</td>
         </tr>
       </tbody>
     </q-markup-table>
@@ -40,7 +43,8 @@
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import copyToClipboard from 'copy-to-clipboard'
+import { computed, ref, watch } from 'vue'
 
 import { useDialogs } from '@/composables/useDialogs'
 import { useSplit } from '@/composables/useSplit'
@@ -51,14 +55,38 @@ import SettlementDialog from './SettlementDialog.vue'
 const { settlements, participantById } = useSplit()
 const { exec } = useDialogs()
 
-const settlementsExtended = computed(() =>
-  settlements.value.map((settlement) => ({
-    ...settlement,
-    preferences: participantById(settlement.receiver).value.preferences
-  }))
-)
+const selection = ref([])
+
+const selectAll = computed({
+  get() {
+    if (settlements.value.length === 0 || selection.value.length === 0) return false
+    if (settlements.value.length === selection.value.length) return true
+    return null
+  },
+  set(value) {
+    selection.value = value ? [...settlements.value] : []
+  }
+})
+
+watch(settlements, () => (selection.value = []))
 
 async function edit(settlement = {}) {
   await exec(SettlementDialog, { settlement })
+}
+
+async function copy() {
+  const scope = selection.value.length > 0 ? selection.value : settlements.value
+  await copyToClipboard(
+    scope
+      .map((settlement) => {
+        const sender = participantById(settlement.sender).value
+        const receiver = participantById(settlement.receiver).value
+        let sentence = `${sender.name} --> ${receiver.name}: ${settlement.amount}`
+        const preferences = receiver.preferences
+        if (preferences && preferences.length > 0) sentence += ` (${preferences})`
+        return sentence
+      })
+      .join('\n')
+  )
 }
 </script>
