e275a3382ea3ddf7bff3e6cc36aa351d6a12d109
Andrew N. Markin <andrew.n.markin@gmail.com>
2026-09-25 03:32:16 +0300

    Amount validation schema improved

--- src/schemas.js
+++ src/schemas.js
@@ -7,4 +7,5 @@ export const amountSchema = z
     message: 'Amount must be a positive number with up to 2 decimal places'
   })
   .transform((value) => Number(value))
-  .refine((value) => value > 0, 'Amount must be greater than 0')
+  .refine((value) => value > 0.01, 'Amount cannot be less than 0.01')
+  .refine((value) => value <= 99999999.99, 'Amount cannot be more than 99999999.99')
